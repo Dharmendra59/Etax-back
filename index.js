256cee0b9bea0c4b98beb7685d0c051a206e6b08
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-02-27 09:31:22 +0530

    sdf

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dbConnection from './models/db.js';
 import AuthRouter from './routes/auth/AuthRouter.js';
 import cookieParser from "cookie-parser"
+import contactRoute from './routes/auth/ContactRouter.js';
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,8 @@ app.use(cors({
 app.use(bodyParser.json());
 
 app.use('/auth', AuthRouter);
+app.use('/contact-data',contactRoute);
+
 
 app.listen(3000, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
