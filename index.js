32bf817b8fcb2c46faf8bf28a2f08fec2efee8c8
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-02-28 06:29:02 +0530

    dffg

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 dbConnection();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-taxgst.netlify.app/",
     credentials: true
 }));
 app.use(bodyParser.json());
