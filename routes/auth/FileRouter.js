99580961d1d0322adf98b68540f3d246664d410d
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-05-28 17:41:22 +0530

    dfg

--- routes/auth/FileRouter.js
+++ routes/auth/FileRouter.js
@@ -8,21 +8,21 @@ import { upload } from '../../middlewares/cloudinaryMiddle.js';
 const router = express.Router();
 
 // Ensure uploads directory exists
-const uploadDir = 'uploads';
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
+// const uploadDir = 'uploads';
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir);
+// }
 
 // Configure multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, uploadDir);
+//     },
+//     filename: (req, file, cb) => {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//         cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+//     }
+// });
 
 // const upload = multer({ storage });
 
