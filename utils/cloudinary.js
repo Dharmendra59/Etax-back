2751d2d3de0d2d51444d23e987741097fa14d708
Dharmendra59 <singhdhas9559604443a@gmail.com>
2025-07-02 21:17:27 +0530

    sahi

--- utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-    cloud_name: "dnrhizw9r",
-    api_key: "754318295319783",
-    api_secret: "3tIh9HO96K7b7JvJPdteYj6nsZc"
-});
-
-export default cloudinary;
